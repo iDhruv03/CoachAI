8848910eec6890962dc81b2b31222688e76a8001
Dhruv <dhruvvv099@gmail.com>
2025-07-01 18:27:13 +0530

    auth logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
+"use client"
+import { SignedIn, SignedOut, SignInButton } from '@clerk/clerk-react'
+import { SignOutButton } from '@clerk/nextjs'
 import React from 'react'
 
 const HomePage = () => {
   return (
     <div>
       HomePage
+      <SignedIn>
+         <SignOutButton />
+      </SignedIn>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
     </div>
   )
 }

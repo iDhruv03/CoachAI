8e8ca1009cf54e74d2181a72238a550a70820738
Dhruv <dhruvvv099@gmail.com>
2025-07-03 20:02:43 +0530

    schema update

--- convex/schema.ts
+++ convex/schema.ts
@@ -9,4 +9,24 @@ export default defineSchema({
         clerkId: v.string(),
 
     }).index("byClerkId", ["clerkId"]),
+
+
+    plans: defineTable({
+        userId: v.id("users"),
+        name: v.string(),
+        workoutPlan: v.object({
+            schedule: v.array(v.string()),
+            exercises: v.array(v.object({
+                day: v.string(),
+                routines: v.array(v.object({
+                    name: v.string(),
+                    sets: v.optional(v.number()),
+                    reps: v.optional(v.number()),
+                    duration: v.optional(v.string()),
+                    description: v.optional(v.string()),
+                    exercises: v.optional(v.array(v.string())),
+                })),
+            })),
+        }),
+    })
 })

18093cdc257d2c6bf05c67a04a1333ed7264e58c
Dhruv <dhruvvv099@gmail.com>
2025-07-05 11:38:18 +0530

    bug fixes

--- convex/http.ts
+++ convex/http.ts
@@ -1,32 +1,38 @@
-import { httpRouter, httpAction } from "convex/server";
-
-import api from "./_generated/api"
-import {Webhook} from "svix";
+import { httpRouter } from "convex/server";
 import { WebhookEvent } from "@clerk/nextjs/server";
+import { Webhook } from "svix";
+import { api } from "./_generated/api";
+import { httpAction } from "./_generated/server";
 
 const http = httpRouter();
 
+
 http.route({
-    path:"/clerk-webhook",
-    method: "POST",
-    handler: httpAction(async(ctx:any, request:Request) => {
-        const webhookSecret = process.env.CLERK_WEBHOOK_SECRET; 
-        if (!webhookSecret) {
-            throw new Error("CLERK_WEBHOOK_SECRET is not set");
-        }
-        const svix_id = request.headers.get("svix-id");
-        const svix_timestamp = request.headers.get("svix-timestamp");
-        const svix_signature = request.headers.get("svix-signature");
+  path: "/clerk-webhook",
+  method: "POST",
+  handler: httpAction(async (ctx, request) => {
+    const webhookSecret = process.env.CLERK_WEBHOOK_SECRET;
+    if (!webhookSecret) {
+      throw new Error("Missing CLERK_WEBHOOK_SECRET environment variable");
+    }
 
-        if (!svix_id || !svix_timestamp || !svix_signature) {
-            throw new Error("Missing svix headers");
-        }
-        const payload = await request.json();
-        const body = JSON.stringify(payload); 
-        const wh = new Webhook(webhookSecret);
-        let evt: WebhookEvent;
+    const svix_id = request.headers.get("svix-id");
+    const svix_signature = request.headers.get("svix-signature");
+    const svix_timestamp = request.headers.get("svix-timestamp");
 
-        try {
+    if (!svix_id || !svix_signature || !svix_timestamp) {
+      return new Response("No svix headers found", {
+        status: 400,
+      });
+    }
+
+    const payload = await request.json();
+    const body = JSON.stringify(payload);
+
+    const wh = new Webhook(webhookSecret);
+    let evt: WebhookEvent;
+
+    try {
       evt = wh.verify(body, {
         "svix-id": svix_id,
         "svix-timestamp": svix_timestamp,
@@ -38,21 +44,35 @@ http.route({
     }
 
     const eventType = evt.type;
-    if(eventType === "user.created") {
-        const {id,first_name,last_name,image_url,email_addresses} = evt.data;
-        const email = email_addresses[0].email_address;
-        const name = `${first_name || ""} ${last_name || ""}`.trim();
-        try {
-            await ctx.runMutation(api.users.syncUser, {
-                email,
-                name,
-                image: image_url,
-                clerkId: id,
-            })
-        } catch (error) {
-            console.log("Error creating user:", error);
-            return new Response("Error creating user", { status: 500 });
-        }
-    } 
-    })
-})
+
+    if (eventType === "user.created") {
+      const { id, first_name, last_name, image_url, email_addresses } = evt.data;
+
+      const email = email_addresses[0].email_address;
+
+      const name = `${first_name || ""} ${last_name || ""}`.trim();
+
+      try {
+        await ctx.runMutation(api.users.syncUser, {
+          email,
+          name,
+          image: image_url,
+          clerkId: id,
+        });
+      } catch (error) {
+        console.log("Error creating user:", error);
+        return new Response("Error creating user", { status: 500 });
+      }
+    }
+
+    if (eventType === "user.updated") {
+      const { id, email_addresses, first_name, last_name, image_url } = evt.data;
+
+      const email = email_addresses[0].email_address;
+      const name = `${first_name || ""} ${last_name || ""}`.trim();
+
+    }
+
+    return new Response("Webhooks processed successfully", { status: 200 });
+  }),
+});
--- convex/users.ts
+++ convex/users.ts
@@ -1,18 +1,20 @@
-import {mutation} from "./_generated/server";
-import {v} from "convex/values";
+import { mutation } from "./_generated/server";
+import { v } from "convex/values";
 
-export const syncUsers = mutation({
-    args: {
-        name: v.string(),
-        email: v.string(),
-        clerkId: v.string(),
-        image: v.optional(v.string()),
+export const syncUser = mutation({
+  args: {
+    name: v.string(),
+    email: v.string(),
+    clerkId: v.string(),
+    image: v.optional(v.string()),
+  },
+  handler: async (ctx, args) => {
+    const existingUser = await ctx.db
+      .query("users")
+      .filter((q) => q.eq(q.field("clerkId"), args.clerkId))
+      .first();
+    if (existingUser) return;
 
-    },
-        handler: async (ctx, args) =>{
-            const existingUser = await ctx.db.query("users").filter(q => q.eq(q.field("clerkId"), args.clerkId)).first();
-            if (existingUser)  return;
-
-            return await ctx.db.insert("users", args);
-        },
-})
+    return await ctx.db.insert("users", args);
+  },
+});

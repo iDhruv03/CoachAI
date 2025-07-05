9893ef7ed907cb4d232ead044233d8a8a64b5b1a
Dhruv <dhruvvv099@gmail.com>
2025-07-05 19:44:12 +0530

    logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,7 @@
 "use client"
 import { useUser } from "@clerk/nextjs"
+import { ZapIcon } from "lucide-react";
+import Link from "next/link";
 
 const Navbar = () => {
     const {isSignedIn} = useUser();
@@ -7,7 +9,15 @@ const Navbar = () => {
   return (
     <header className="fixed top-0 left-0 right-0 z-50 bg-background/60 backdrop-blur-md border-b border-border py-3">
       <div className = "container mx-auto flex items-center justify-between">
-
+    {/* LOGO */}
+    <Link href="/" className="flex items-center gap-2">
+          <div className="p-1 bg-primary/10 rounded">
+            <ZapIcon className="w-4 h-4 text-primary" />
+          </div>
+          <span className="text-xl font-bold font-mono">
+            code<span className="text-primary">flex</span>.ai
+          </span>
+        </Link>
       </div>
     </header>
   )

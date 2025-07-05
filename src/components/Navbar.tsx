8fa95533b0c966b741e9bac85604e4673bd54584
Dhruv <dhruvvv099@gmail.com>
2025-07-05 18:51:29 +0530

    navbar update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+"use client"
+import { useUser } from "@clerk/nextjs"
 
 const Navbar = () => {
+    const {isSignedIn} = useUser();
+
   return (
-    <div>Navbar</div>
+    <header className="fixed top-0 left-0 right-0 z-50 bg-background/60 backdrop-blur-md border-b border-border py-3">
+      <div className = "container mx-auto flex items-center justify-between">
+
+      </div>
+    </header>
   )
 }
 
-export default Navbar
+export default Navbar;

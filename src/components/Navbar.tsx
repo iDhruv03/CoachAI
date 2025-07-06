d6db16fd3d908297e195586edb7d26de70b16886
Dhruv <dhruvvv099@gmail.com>
2025-07-06 12:58:06 +0530

    navbar styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,8 @@
 "use client"
-import { useUser } from "@clerk/nextjs"
-import { ZapIcon } from "lucide-react";
+import { SignInButton, SignUpButton, UserButton, useUser } from "@clerk/nextjs"
+import { DumbbellIcon, HomeIcon, UserIcon, ZapIcon } from "lucide-react";
 import Link from "next/link";
+import { Button } from "./ui/button";
 
 const Navbar = () => {
     const {isSignedIn} = useUser();
@@ -15,9 +16,64 @@ const Navbar = () => {
             <ZapIcon className="w-4 h-4 text-primary" />
           </div>
           <span className="text-xl font-bold font-mono">
-            code<span className="text-primary">flex</span>.ai
+            <span className="text-primary">Coach</span>.ai
           </span>
         </Link>
+
+        <nav className="flex items-center gap-5">
+                {isSignedIn ? (
+            <>
+              <Link
+                href="/"
+                className="flex items-center gap-1.5 text-sm hover:text-primary transition-colors"
+              >
+                <HomeIcon size={16} />
+                <span>Home</span>
+              </Link>
+
+              <Link
+                href="/generate-program"
+                className="flex items-center gap-1.5 text-sm hover:text-primary transition-colors"
+              >
+                <DumbbellIcon size={16} />
+                <span>Generate</span>
+              </Link>
+
+              <Link
+                href="/profile"
+                className="flex items-center gap-1.5 text-sm hover:text-primary transition-colors"
+              >
+                <UserIcon size={16} />
+                <span>Profile</span>
+              </Link>
+              <Button
+                asChild
+                variant="outline"
+                className="ml-2 border-primary/50 text-primary hover:text-white hover:bg-primary/10"
+              >
+                <Link href="/generate-program">Get Started</Link>
+              </Button>
+              <UserButton />
+            </>
+          ) : (
+            <>
+              <SignInButton>
+                <Button
+                  variant={"outline"}
+                  className="border-primary/50 text-primary hover:text-white hover:bg-primary/10"
+                >
+                  Sign In
+                </Button>
+              </SignInButton>
+
+              <SignUpButton>
+                <Button className="bg-primary text-primary-foreground hover:bg-primary/90">
+                  Sign Up
+                </Button>
+              </SignUpButton>
+            </>
+          )}
+        </nav>
       </div>
     </header>
   )

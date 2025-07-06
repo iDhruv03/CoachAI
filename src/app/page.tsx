058fbafd0a88b5d7fc470993396b7cb9835e2078
Dhruv <dhruvvv099@gmail.com>
2025-07-06 13:23:45 +0530

    homepage UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,20 @@
-"use client"
-import { SignedIn, SignedOut, SignInButton } from '@clerk/clerk-react'
-import { SignOutButton } from '@clerk/nextjs'
-import React from 'react'
+
 
 const HomePage = () => {
   return (
-    <div>
-      HomePage
-      <SignedIn>
-         <SignOutButton />
-      </SignedIn>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
+    <div className="flex flex-col min-h-screen text-foreground overflow-hidden">
+     <section className="relative z-10 py-24 flex-grow">
+      <div className="container mx-auto px-4">
+         <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 items-center relative">
+          {/* CORNER declaration */}
+          <div className="absolute top-10 left-0 w-40 h-40 border-l-2 border-t-2 border-border" />
+
+          
+
+         </div>
+      </div>
+     </section>
+      
     </div>
   )
 }

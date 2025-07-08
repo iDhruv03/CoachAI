267466e04f81ebb881b26e89df8a641744f9352d
Dhruv <dhruvvv099@gmail.com>
2025-07-08 17:57:10 +0530

    LLM integration

--- convex/http.ts
+++ convex/http.ts
@@ -3,9 +3,11 @@ import { WebhookEvent } from "@clerk/nextjs/server";
 import { Webhook } from "svix";
 import { api } from "./_generated/api";
 import { httpAction } from "./_generated/server";
+import {GoogleGenerativeAI} from "@google/generative-ai";
 
 const http = httpRouter();
 
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
 
 http.route({
   path: "/clerk-webhook",
@@ -95,8 +97,18 @@ http.route({
         fitness_level,
         dietary_restrictions,
       } = payload;
+
+      const model = genAI.getGenerativeModel({
+        model: "gemini-2.0-flash-001",
+        generationConfig: {
+          temperature: 0.4, // lower temperature for more predictable outputs
+          topP: 0.9,
+          responseMimeType: "application/json",
+        },
+      });
+
     } catch (error) {
-      
+      console.log("Error creating the plan");
     }
   })
 })

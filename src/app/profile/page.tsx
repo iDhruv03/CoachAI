2b7f6350ee4dd621c9cae407cacbafd8e3565e8f
Dhruv <dhruvvv099@gmail.com>
2025-07-09 13:06:59 +0530

    current and active plan toggle

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+"use client";
+
+import { useUser } from '@clerk/nextjs';
+import { useQuery } from 'convex/react';
+import React, { useState } from 'react'
+import { api } from '../../../convex/_generated/api';
 
 const ProfilePage = () => {
-  return (
-    <div>ProfilePage</div>
-  )
+
+  const {user} = useUser();
+  const userId = user?.id as string;
+
+  const allPlan = useQuery(api.plans.getUserPlans, {userId});
+  const {selectedPlanId, setSelectedPlanId} = useState<null | string>(null);
+  const activePlan = allPlans?.find(plan => plan.isActive);
+
+  const currentPlan = selectedPlanId ? allPlans?.find(plan => plan._id === selectedPlanId) : activePlan;
+  return <div>ProfilePage</div>
+  
 }
 
 export default ProfilePage
